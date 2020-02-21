b0fad149ff5085be8392544896fc2e8bc7e0771c
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-02-21 12:39:27 +0100

    Added default constructor for CustomSharedPointer

--- include/custom_shared_pointer.hpp
+++ include/custom_shared_pointer.hpp
@@ -3,8 +3,22 @@
 template<typename T>
 class CustomSharedPointer
 {
+private:
+    T* heap_ptr_;
+    int* nbr_owners_;
 public:
     CustomSharedPointer()
+        :heap_ptr_{nullptr}, nbr_owners_{nullptr}{
+
+    }
+
+    T* get()
     {
+        return heap_ptr_;
+    }
+
+    int get_nbr_owners()
+    {
+        return *nbr_owners_;
     }
 };
--- test/test_custom_shared_pointer.cpp
+++ test/test_custom_shared_pointer.cpp
@@ -1,2 +1,9 @@
 #include "gtest/gtest.h"
 #include "custom_shared_pointer.hpp"
+
+TEST(TestCustomSharedPointer, DefaultConstructionTest)
+{
+	auto shared_ptr = CustomSharedPointer<int>();
+
+	ASSERT_EQ(shared_ptr.get(), nullptr);
+}

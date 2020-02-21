966bd454b4c4cc515431bff5d5ccc0f0c74fe29f
mohamed lmoutaouakil <lmoutaouakilmohamed@gmail.com>
2020-02-21 11:54:21 +0100

    Added googletest framework for unit testing

---
+++ include/custom_shared_pointer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+template<typename T>
+class CustomSharedPointer
+{
+public:
+    CustomSharedPointer()
+    {
+    }
+};
---
+++ test/test_custom_shared_pointer.cpp
@@ -0,0 +1,2 @@
+#include "gtest/gtest.h"
+#include "custom_shared_pointer.hpp"
